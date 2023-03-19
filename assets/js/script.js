e1c4d5ddef04a8e676d23a8a24d59fa2caa68bba
AdrianMuntean97 <adi_muntean_13@yahoo.com>
2023-03-19 20:15:50 +0000

    Modified the structure of html and implemented the function nextQuestion

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,25 +4,37 @@
 const question = document.getElementById("question");
 const fiftyFifty = document.getElementById("fifty-fifty");
 const extraTime = document.getElementById("extra-time");
-const answerButtons = document.getElementsByClassName("answer-btn");
 const submitButton = document.getElementById("submit-btn");
 const nextButton = document.getElementById("next-btn");
 const name = document.getElementById("name");
 const score = document.getElementById("score");
-
+const answerButtons = document.getElementById("answer-buttons");
 
 document.addEventListener("DOMContentLoaded", function() {
-
+ nextQuestion();
 });
 
 function nextQuestion() {
-  let currentQuestion = question.getAttribute(data-indexQuestion);
+  let currentQuestion = question.getAttribute("data-indexQuestion");
   if (currentQuestion === "") {
     currentQuestion = 0; 
   } else {
     currentQuestion++;
   }
+
+    const questionData = questions[currentQuestion];
+    question.textContent = questionData["question"];
+    question.setAttribute("data-indexQuestion", currentQuestion);
+    
+    let variants = "";
+    let options = questionData["options"];
+    for (let option of options) {
+        variants += `<button class="answer-btn">${option}</button>`;
+    }
+    answerButtons.innerHTML = variants;
 };
+
+
 
 function confirmAnswer(){
 
