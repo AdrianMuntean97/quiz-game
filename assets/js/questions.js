289f647224eef070d361755daaa7ba845190b0fa
AdrianMuntean97 <adi_muntean_13@yahoo.com>
2023-03-19 19:08:11 +0000

    Add questions array in questions.js

---
+++ assets/js/questions.js
@@ -0,0 +1,152 @@
+const questions = [
+      {
+        "question": "What is the capital city of France?",
+        "options": ["London", "Paris", "Rome", "Madrid"],
+        "answer": "Paris"
+      },
+      {
+        "question": "What is the largest ocean in the world?",
+        "options": ["Atlantic Ocean", "Indian Ocean", "Arctic Ocean", "Pacific Ocean"],
+        "answer": "Pacific Ocean"
+      },
+      {
+        "question": "Who is the author of the Harry Potter book series?",
+        "options": ["J.K. Rowling", "Stephenie Meyer", "Suzanne Collins", "Veronica Roth"],
+        "answer": "J.K. Rowling"
+      },
+      {
+        "question": "What is the smallest country in the world by land area?",
+        "options": ["Vatican City", "Monaco", "Nauru", "Tuvalu"],
+        "answer": "Vatican City"
+      },
+      {
+        "question": "What is the highest mountain in the world?",
+        "options": ["Mount Everest", "K2", "Kangchenjunga", "Lhotse"],
+        "answer": "Mount Everest"
+      },
+      {
+        "question": "What is the currency of Japan?",
+        "options": ["Yen", "Euro", "Dollar", "Pound"],
+        "answer": "Yen"
+      },
+      {
+        "question": "Who painted the famous artwork 'The Starry Night'?",
+        "options": ["Vincent van Gogh", "Pablo Picasso", "Leonardo da Vinci", "Rembrandt"],
+        "answer": "Vincent van Gogh"
+      },
+      {
+        "question": "What is the name of the longest river in Africa?",
+        "options": ["Nile River", "Congo River", "Zambezi River", "Orange River"],
+        "answer": "Nile River"
+      },
+      {
+        "question": "What is the chemical symbol for gold?",
+        "options": ["Au", "Ag", "Cu", "Fe"],
+        "answer": "Au"
+      },
+      {
+        "question": "What is the name of the largest desert in the world?",
+        "options": ["Sahara", "Arabian", "Gobi", "Antarctica"],
+        "answer": "Sahara"
+      },
+      {
+        "question": "Which country has the largest population in the world?",
+        "options": ["China", "India", "United States", "Russia"],
+        "answer": "China"
+      },
+      {
+        "question": "Who is the current President of the United States?",
+        "options": ["Donald Trump", "Joe Biden", "Barack Obama", "George W. Bush"],
+        "answer": "Joe Biden"
+      },
+      {
+        "question": "What is the name of the world's largest reef system?",
+        "options": ["Great Barrier Reef", "Coral Triangle", "Hawaiian Coral Reef", "Red Sea Coral Reef"],
+        "answer": "Great Barrier Reef"
+      },
+      {
+        "question": "What is the name of the highest waterfall in the world?",
+        "options": ["Angel Falls", "Iguazu Falls", "Niagara Falls", "Victoria Falls"],
+        "answer": "Angel Falls"
+      },
+      {
+        "question": "What is the name of the planet that is closest to the sun?",
+        "options": ["Mercury", "Venus", "Mars", "Jupiter"],
+        "answer": "Mercury"
+      },
+      {
+        "question": "What is the name of the largest planet in our solar system?",
+        "options": ["Jupiter", "Saturn", "Uranus", "Neptune"],
+        "answer": "Jupiter"
+      },
+      {
+        "question": "What is the name of the smallest planet in our solar system?",
+        "options": ["Mercury", "Venus", "Mars", "Earth"],
+        "answer": "Mercury"
+      },
+      {
+        "question": "What is the name of the world's largest hot desert?",
+        "options": ["Sahara Desert", "Arabian Desert", "Gobi Desert", "Kalahari Desert"],
+        "answer": "Sahara Desert"
+      },
+      {
+        "question": "What is the name of the world's largest cold desert?",
+        "options": ["Antarctica", "Arctic", "Gobi", "Atacama"],
+        "answer": "Antarctica"
+      },
+      {
+        "question": "What is the name of the largest lake in Africa?",
+        "options": ["Lake Victoria", "Lake Tanganyika", "Lake Malawi", "Lake Chad"],
+        "answer": "Lake Victoria"
+      },
+      {
+        "question": "What is the capital of Japan?",
+        "options": ["Kyoto", "Tokyo", "Osaka", "Hiroshima"],
+        "answer": "Tokyo"
+      },
+      {
+        "question": "What is the name of the smallest country in the world?",
+        "options": ["San Marino", "Liechtenstein", "Monaco", "Vatican City"],
+        "answer": "Vatican City"
+      },
+      {
+        "question": "What is the name of the world's largest ocean?",
+        "options": ["Pacific Ocean", "Atlantic Ocean", "Indian Ocean", "Arctic Ocean"],
+        "answer": "Pacific Ocean"
+      },
+      {
+        "question": "What is the name of the world's largest volcano?",
+        "options": ["Mauna Loa", "Krakatoa", "Mount St. Helens", "Mount Everest"],
+        "answer": "Mauna Loa"
+      },
+      {
+        "question": "What is the name of the highest mountain in Africa?",
+        "options": ["Mount Kilimanjaro", "Mount Kenya", "Mount Meru", "Mount Elgon"],
+        "answer": "Mount Kilimanjaro"
+      },
+      {
+        "question": "What is the name of the longest river in the world?",
+        "options": ["Nile River", "Amazon River", "Yangtze River", "Mississippi River"],
+        "answer": "Nile River"
+      },
+      {
+        "question": "What is the name of the world's largest waterfall by volume?",
+        "options": ["Niagara Falls", "Victoria Falls", "Iguazu Falls", "Angel Falls"],
+        "answer": "Victoria Falls"
+      },
+      {
+        "question": "What is the name of the world's largest island?",
+        "options": ["Greenland", "Australia", "Madagascar", "Borneo"],
+        "answer": "Greenland"
+      },
+      {
+        "question": "What is the name of the world's largest canyon?",
+        "options": ["Grand Canyon", "Fish River Canyon", "Yarlung Tsangpo Grand Canyon", "Blyde River Canyon"],
+        "answer": "Yarlung Tsangpo Grand Canyon"
+      },
+      {
+        "question": "What is the name of the world's largest flower?",
+        "options": ["Rafflesia", "Corpse Flower", "Titan Arum", "Lily"],
+        "answer": "Rafflesia"
+      }
+    ]
