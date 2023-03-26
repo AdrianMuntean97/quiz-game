3da8ffa303162e24fdc49679e1207cbda0a1ab90
AdrianMuntean97 <adi_muntean_13@yahoo.com>
2023-03-26 17:42:51 +0000

    Rearrange javascript code to make it more readeable

--- assets/js/script.js
+++ assets/js/script.js
@@ -59,126 +59,6 @@ document.getElementById("feedback-submit").addEventListener("click", function() 
 })
 });
 
-function nextQuestion() {
-  let currentQuestion = question.getAttribute("data-indexQuestion");
-  if (currentQuestion === "") {
-    currentQuestion = 0; 
-  } else if(currentQuestion < questions.length - 1) {
-    currentQuestion++;
-  } else {
-    endGame();
-    return;
-  }
-
-    const questionData = questions[currentQuestion];
-    question.textContent = questionData.question;
-    question.setAttribute("data-indexQuestion", currentQuestion);
-
-    let variants = "";
-    let options = questionData.options;
-    for (let option of options) {
-        variants += `<button class="answer-btn">${option}</button>`;
-    }
-
-    answerButtons.innerHTML = variants;
-    
-    nextButton.classList.add("hidden");
-    submitButton.classList.remove("hidden");
-    clickedAnswer();
-}
-
-function checkAnswer(){
-    if (document.getElementsByClassName("selected").length === 0) {
-        return;
-    }
-
-    let currentQuestion = question.getAttribute("data-indexQuestion");
-    const rightAnswer = questions[currentQuestion].answer;
-
-    let selectedAnswer = null;
-    const answers = document.getElementsByClassName("answer-btn");
-    for (const answer of answers) {
-       if (answer.classList.contains("selected")) {
-        selectedAnswer = answer;
-       } else {
-        answer.setAttribute("disabled", true);
-        if (answer.innerHTML === rightAnswer) {
-            answer.classList.add("correct-answer");
-        }
-       }
-    }
-
-    if(rightAnswer === selectedAnswer.innerHTML) {
-        selectedAnswer.classList.remove("selected");
-        selectedAnswer.classList.add("correct-answer");
-        nextButton.classList.remove("hidden");
-        submitButton.classList.add("hidden");
-        incrementScore();
-
-    } else {
-        selectedAnswer.classList.remove("selected");
-        selectedAnswer.classList.add("wrong-answer");
-        nextButton.classList.remove("hidden");
-        submitButton.classList.add("hidden");
-    }
-}
-
-function clickedAnswer() {
-    const answers = document.getElementsByClassName("answer-btn");
-    for (const answer of answers) {
-        answer.addEventListener("click", function() {
-            for (const selected of answers) {
-                selected.classList.remove("selected");
-            }
-            this.classList.add("selected");
-        });
-        answer.addEventListener("keydown", function() {
-          if (submitButton.classList.contains("hidden") === false) {
-            submitButton.click();
-          } else {
-            nextButton.click();
-          }
-        });
-    }
-}
-
-function endGame() {
-  question.textContent = `Congratulation!`;
-  fiftyFifty.remove();
-  extraTime.remove();
-  answerButtons.remove();
-  submitButton.remove();
-  nextButton.remove();
-  newGame.classList.remove("hidden");
-  feedbackContent.classList.remove("hidden");
-  score.textContent = `You answered correctly to ${score.innerText} out of ${questions.length} questions!`;
-}
-
-function incrementScore() {
-  score.textContent = parseInt(score.innerText) + 1;
-}
-
-function shuffleQuestions() {
-  shuffleArray(questions);
-  for (const elem of questions) {
-    shuffleArray(elem.options);
-  }
-}
-
-function shuffleArray(array) {
-  const arrayLength = array.length;
-
-  for (let i = 0; i < arrayLength; i++) {
-
-    const randomIndex = Math.floor(Math.random() * arrayLength);
-
-    const temp = array[i];
-    array[i] = array[randomIndex];
-    array[randomIndex] = temp;
-  }
-
-}
-
 function fiftyFiftyFunction() {
   const questionIndex = question.getAttribute("data-indexQuestion");
   const questionData = questions[questionIndex];
@@ -219,3 +99,124 @@ function timerFunction() {
 function extraTimeFunction() {
   time += 120;
 }
+
+function shuffleQuestions() {
+  shuffleArray(questions);
+  for (const elem of questions) {
+    shuffleArray(elem.options);
+  }
+}
+
+function nextQuestion() {
+  let currentQuestion = question.getAttribute("data-indexQuestion");
+  if (currentQuestion === "") {
+    currentQuestion = 0; 
+  } else if(currentQuestion < questions.length - 1) {
+    currentQuestion++;
+  } else {
+    endGame();
+    return;
+  }
+
+    const questionData = questions[currentQuestion];
+    question.textContent = questionData.question;
+    question.setAttribute("data-indexQuestion", currentQuestion);
+
+    let variants = "";
+    let options = questionData.options;
+    for (let option of options) {
+        variants += `<button class="answer-btn">${option}</button>`;
+    }
+
+    answerButtons.innerHTML = variants;
+    
+    nextButton.classList.add("hidden");
+    submitButton.classList.remove("hidden");
+    clickedAnswer();
+}
+
+function clickedAnswer() {
+  const answers = document.getElementsByClassName("answer-btn");
+  for (const answer of answers) {
+      answer.addEventListener("click", function() {
+          for (const selected of answers) {
+              selected.classList.remove("selected");
+          }
+          this.classList.add("selected");
+      });
+      answer.addEventListener("keydown", function() {
+        if (submitButton.classList.contains("hidden") === false) {
+          submitButton.click();
+        } else {
+          nextButton.click();
+        }
+      });
+  }
+}
+
+function checkAnswer(){
+    if (document.getElementsByClassName("selected").length === 0) {
+        return;
+    }
+
+    let currentQuestion = question.getAttribute("data-indexQuestion");
+    const rightAnswer = questions[currentQuestion].answer;
+
+    let selectedAnswer = null;
+    const answers = document.getElementsByClassName("answer-btn");
+    for (const answer of answers) {
+       if (answer.classList.contains("selected")) {
+        selectedAnswer = answer;
+       } else {
+        answer.setAttribute("disabled", true);
+        if (answer.innerHTML === rightAnswer) {
+            answer.classList.add("correct-answer");
+        }
+       }
+    }
+
+    if(rightAnswer === selectedAnswer.innerHTML) {
+        selectedAnswer.classList.remove("selected");
+        selectedAnswer.classList.add("correct-answer");
+        nextButton.classList.remove("hidden");
+        submitButton.classList.add("hidden");
+        incrementScore();
+
+    } else {
+        selectedAnswer.classList.remove("selected");
+        selectedAnswer.classList.add("wrong-answer");
+        nextButton.classList.remove("hidden");
+        submitButton.classList.add("hidden");
+    }
+}
+
+function incrementScore() {
+  score.textContent = parseInt(score.innerText) + 1;
+}
+
+function endGame() {
+  question.textContent = `Congratulation!`;
+  fiftyFifty.remove();
+  extraTime.remove();
+  answerButtons.remove();
+  submitButton.remove();
+  nextButton.remove();
+  newGame.classList.remove("hidden");
+  feedbackContent.classList.remove("hidden");
+  score.textContent = `You answered correctly to ${score.innerText} out of ${questions.length} questions!`;
+}
+
+function shuffleArray(array) {
+  const arrayLength = array.length;
+
+  for (let i = 0; i < arrayLength; i++) {
+
+    const randomIndex = Math.floor(Math.random() * arrayLength);
+
+    const temp = array[i];
+    array[i] = array[randomIndex];
+    array[randomIndex] = temp;
+  }
+
+}
+
